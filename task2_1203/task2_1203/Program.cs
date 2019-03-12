7ce9743016854074a4113b627f26aa5d0643c3f0
Helen Pribysh <helenpribysh@yandex.com>
2019-03-12 21:32:05 +0300

    Switch task

---
+++ task2_1203/task2_1203/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task2_1203
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Color color = (Color)(new System.Random().Next(0, 3));
+
+            //Console.WriteLine(color);
+
+            switch (color)
+            {
+                //case Color.Red:
+                //    Console.WriteLine("THE COLOR IS RED");
+                //    break;
+                //case Color.Green:
+                //    Console.WriteLine("THE COLOR IS GREEN");
+                //    break;
+                //case Color.Blue:
+                //    Console.WriteLine("THE COLOR IS BLUE");
+                //    break;
+                default:
+                    Console.WriteLine("NO COLOR DEFINED");
+                    break;
+            }
+
+            //switch (color)
+            //{
+            //    case Color.Red:
+            //        Console.WriteLine("THE COLOR IS RED");
+            //        break;
+            //    case Color.Green:
+            //        break;
+            //}
+
+            Console.Read();
+        }
+    }
+
+    public enum Color { Red, Green, Blue }
+
+    
+}
