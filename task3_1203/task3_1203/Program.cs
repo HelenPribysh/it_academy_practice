42c3106bad036c454966f8ab5790de61fa600642
Helen Pribysh <helenpribysh@yandex.com>
2019-03-12 21:39:50 +0300

    Letter task

---
+++ task3_1203/task3_1203/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task3_1203
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            int count = 0;
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (object.Equals(word[i], 'a'))
+                    count += 1;
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
