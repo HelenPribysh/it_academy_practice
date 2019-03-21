c3ab94a8248033fd5c511486f47a179dbb29e143
Helen Pribysh <helenpribysh@gmail.com>
2019-03-21 20:11:24 +0300

    task2

--- task1_2103/task1_2103/Program.cs
+++ task1_2103/task1_2103/Program.cs
@@ -12,12 +12,27 @@ namespace task1_2103
         {
             //task1
             EmptyArray();
+
+            //task2
+            ArrayOfDifferentSymbols();
         }
 
         public static void EmptyArray()
         {
             int[] emptyArray = new int[0];
             Console.WriteLine(emptyArray.Length);
+        }
+
+        public static void ArrayOfDifferentSymbols()
+        {
+            string[] arrayOfDifferentSymbols = new string[] { "10", "A", "Hello" };
+            foreach (string i in arrayOfDifferentSymbols)
+                Console.WriteLine(i);
+
+            arrayOfDifferentSymbols[0] = Convert.ToString((Convert.ToInt32(arrayOfDifferentSymbols[0]) + 10));
+            arrayOfDifferentSymbols[2] += ", guys!";
+            foreach (string i in arrayOfDifferentSymbols)
+                Console.WriteLine(i);
         }
     }
 }
