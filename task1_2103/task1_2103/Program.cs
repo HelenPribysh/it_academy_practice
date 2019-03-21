ca69d72cac235c627909f52a3a1b808e209e1db0
Helen Pribysh <helenpribysh@gmail.com>
2019-03-21 19:48:09 +0300

    task1

---
+++ task1_2103/task1_2103/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1_2103
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //task1
+            EmptyArray();
+        }
+
+        public static void EmptyArray()
+        {
+            int[] emptyArray = new int[0];
+            Console.WriteLine(emptyArray.Length);
+        }
+    }
+}
