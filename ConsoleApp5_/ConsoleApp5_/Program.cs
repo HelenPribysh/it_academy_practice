48fde013663f4d97fbe446bf763067ad245dd667
Helen Pribysh <helenpribysh@gmail.com>
2019-04-16 21:12:34 +0300

    Implemented task

---
+++ ConsoleApp5_/ConsoleApp5_/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Uniqueltem uniqueltem0 = new Uniqueltem();
+            Console.WriteLine(uniqueltem0.Id);
+            Uniqueltem uniqueltem1 = new Uniqueltem();
+            Console.WriteLine(uniqueltem1.Id);
+        }
+    }
+
+    public class Uniqueltem
+    {
+        static private int _id;
+        public int Id { get => _id; set => _id = value; }
+
+        public Uniqueltem()
+        {
+            _id += 1;
+        }
+    }
+}
